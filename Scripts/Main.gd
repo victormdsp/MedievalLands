8df2ee031bdedf5ddecabf0e10e699a2fa583f4a
victormdsp <victormdsp@hotmail.com>
2019-10-01 12:49:40 -0300

    Teste upgrades

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -22,6 +22,7 @@ func new_game():
 	$Populacao/Button/AnimatedSprite.show()
 	$Populacao/Button/Label.show()
 	$Populacao/Button.show()
+	$Populacao/PopulacaoLabel.text = str("Population: ",$Populacao.quantidade)
 	$Recursos/Button/AnimatedSprite.show()
 	$Recursos/Button/Label.show()
 	$Recursos/Button.show()
@@ -30,8 +31,6 @@ func new_game():
 	$Upgrades/Button.show()
 	$HUD/Time.show()
 	$HUD/Label.show()
-	$HUD/populacao.text = str("População: ",$Populacao.quantidade)
-	$HUD/populacao.show()
 	$HUD/Label.text = "0" #Início do ano como 0 
 	$Timer.start() #Start do tempo 
 	$Button.show()
@@ -45,7 +44,7 @@ func _on_Timer_timeout():
 		armazem2 = nascer() #Chamda da função nascer e armazena na variável aux2
 		armazem1 = evento(_randomize_my_variable(event),armazem2) #Chamda da função evento juntamente com a função de randomizar uma variável
 		auxpop = populacao(armazem1,armazem2) #Chamada da função para atualizar população
-		$HUD/populacao.text =  str("População: ",$Populacao.quantidade) 
+		$Populacao/PopulacaoLabel.text =  str("Population: ",$Populacao.quantidade) 
 		cont += 10 #Contador aumenta para mantes de 10 em 10 anos
 			
 #Sinal para o start do jogo 
